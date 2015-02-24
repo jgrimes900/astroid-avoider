f8eb0ef078ab17104a2e7d6eabad8544ab201971
jgrimes900 <jgrimes900@yahoo.com>
2015-02-23 17:51:49 -0800

    Create ScreenTitle.java

---
+++ ScreenTitle.java
@@ -0,0 +1,21 @@
+public class ScreenFix extends Screen{
+
+    public ScreenFix(ScreenFactory screenFactory) {
+        super(screenFactory);
+    }
+
+    @Override
+    public void onCreate() {
+    }
+
+    @Override
+    public void onUpdate() {
+    }
+
+    @Override
+    public void onDraw(Graphics2D g2d) {
+    }
+    
+    
+    
+}
